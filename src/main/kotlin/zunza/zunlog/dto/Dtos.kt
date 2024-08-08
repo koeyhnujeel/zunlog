88966d9822d5c8e06c2428d7a994e1a10fbea7a1
zunza <fksfks015@naver.com>
2024-08-09 00:45:37 +0900

    feat: PostDTO 정적 팩토리 메서드 추가

--- src/main/kotlin/zunza/zunlog/dto/Dtos.kt
+++ src/main/kotlin/zunza/zunlog/dto/Dtos.kt
@@ -1,5 +1,6 @@
 package zunza.zunlog.dto
 
+import zunza.zunlog.model.Post
 import java.time.Instant
 
 data class CreatePostDTO(
@@ -15,4 +16,17 @@ data class PostDTO(
     val writer: String,
     val createdDt: Instant,
     val updatedDt: Instant
-)
+) {
+    companion object {
+        fun from(post: Post): PostDTO {
+            return PostDTO(
+                id = post.id,
+                title = post.title,
+                content = post.content,
+                writer = post.writer,
+                createdDt = post.createdDt,
+                updatedDt = post.updatedDt
+            )
+        }
+    }
+}
