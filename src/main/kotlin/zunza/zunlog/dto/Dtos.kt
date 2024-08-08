157d5761a45535ee99aa281c38a32fab5a44f1a1
zunza <fksfks015@naver.com>
2024-08-09 00:29:59 +0900

    feat: PostDTO 추가

--- src/main/kotlin/zunza/zunlog/dto/Dtos.kt
+++ src/main/kotlin/zunza/zunlog/dto/Dtos.kt
@@ -1,7 +1,18 @@
 package zunza.zunlog.dto
 
+import java.time.Instant
+
 data class CreatePostDTO(
     val title: String,
     val content: String,
     val writer: String
+)
+
+data class PostDTO(
+    val id: Long,
+    val title: String,
+    val content: String,
+    val writer: String,
+    val createdDt: Instant,
+    val updatedDt: Instant
 )
