3d2f51668ecc60c1b1a92aefc910752625fb64bf
zunza <fksfks015@naver.com>
2024-08-12 20:59:20 +0900

    feat: setter를 막기 위한 접근제어자 추가 update 메서드 추가

--- src/main/kotlin/zunza/zunlog/model/Models.kt
+++ src/main/kotlin/zunza/zunlog/model/Models.kt
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import zunza.zunlog.dto.CreatePostDTO
+import zunza.zunlog.dto.UpdatePostDTO
 import java.time.Instant
 
 @Entity
@@ -12,12 +13,17 @@ class Post(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0,
-    var title: String,
-    var content: String,
+    private var title: String,
+    private var content: String,
     val writer: String,
     var createdDt: Instant = Instant.now(),
     var updatedDt: Instant = Instant.now(),
 ) {
+    fun update(updatePostDTO: UpdatePostDTO) {
+        this.title = updatePostDTO.title
+        this.content = updatePostDTO.content
+    }
+
     companion object {
         fun from(createPostDTO: CreatePostDTO): Post {
             return Post(
