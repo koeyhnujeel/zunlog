86fce47fd4e880f79022af54ee338803177cf295
zunza <fksfks015@naver.com>
2024-08-12 22:09:34 +0900

    feat: JPA 감사 활성화 및 게시글 업데이트 시간에 적용

---
+++ src/main/kotlin/zunza/zunlog/config/DataConfig.kt
@@ -0,0 +1,9 @@
+package zunza.zunlog.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class DataConfig {
+}
--- src/main/kotlin/zunza/zunlog/model/Models.kt
+++ src/main/kotlin/zunza/zunlog/model/Models.kt
@@ -1,14 +1,15 @@
 package zunza.zunlog.model
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import zunza.zunlog.dto.CreatePostDTO
 import zunza.zunlog.dto.UpdatePostDTO
 import java.time.Instant
 
 @Entity
+@EntityListeners(AuditingEntityListener::class)
 class Post(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,7 +17,8 @@ class Post(
     var title: String,
     var content: String,
     val writer: String,
-    var createdDt: Instant = Instant.now(),
+    val createdDt: Instant = Instant.now(),
+    @LastModifiedDate
     var updatedDt: Instant = Instant.now(),
 ) {
 
