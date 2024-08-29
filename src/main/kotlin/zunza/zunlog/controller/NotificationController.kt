2f8ac73618780a8630a80b6b1186cf94b3589daf
zunza <fksfks015@naver.com>
2024-08-30 00:58:13 +0900

    feat: 읽지 않은 알림 개수 API 추가 알림 목록 API 추가 알림 읽음 처리(개별) API 추가 modify: notify() 메서드 수정 -> 알림 배치 저장

--- src/main/kotlin/zunza/zunlog/controller/NotificationController.kt
+++ src/main/kotlin/zunza/zunlog/controller/NotificationController.kt
@@ -1,18 +1,55 @@
 package zunza.zunlog.controller
 
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
+import org.springframework.http.HttpStatus
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
 import zunza.zunlog.config.UserDetails
+import zunza.zunlog.dto.NotificationDTO
+import zunza.zunlog.dto.UnreadNotificationCountDTO
 import zunza.zunlog.service.NotificationService
 
 @RestController
+@RequestMapping("/notifications")
 class NotificationController(
     private val notificationService: NotificationService
 ) {
     @GetMapping(value = ["/connect"], produces = ["text/event-stream"])
     fun subscribe(@AuthenticationPrincipal userDetails: UserDetails): SseEmitter {
         return notificationService.subscribe(userDetails.getUserId())
+    }
+
+    @GetMapping("/unread/count")
+    @ResponseStatus(HttpStatus.OK)
+    fun getUnreadNotificationCount(@AuthenticationPrincipal userDetails: UserDetails): UnreadNotificationCountDTO {
+        return notificationService.getUnreadNotificationCount(userDetails.getUserId())
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    fun getNotifications(
+        @AuthenticationPrincipal userDetails: UserDetails,
+        @RequestParam page: Int = 1,
+    ): Page<NotificationDTO> {
+
+        val pageable = PageRequest.of(page - 1, 10, Sort.by(Sort.Direction.DESC, "createdDt"))
+        return notificationService.getAllNotifications(userDetails.getUserId(), pageable)
+    }
+
+    @PostMapping("/{notificationId}/read")
+    @ResponseStatus(HttpStatus.OK)
+    fun readNotification(
+        @PathVariable notificationId: Long
+    ) {
+        notificationService.read(notificationId)
     }
 }
--- src/main/kotlin/zunza/zunlog/service/NotificationService.kt
+++ src/main/kotlin/zunza/zunlog/service/NotificationService.kt
@@ -1,8 +1,17 @@
 package zunza.zunlog.service
 
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
+import zunza.zunlog.dto.NotificationDTO
+import zunza.zunlog.dto.UnreadNotificationCountDTO
+import zunza.zunlog.event.CustomEvent
+import zunza.zunlog.exception.NotificationNotFoundException
+import zunza.zunlog.model.Notification
 import zunza.zunlog.model.Subscription
+import zunza.zunlog.repository.NotificationRepository
 import zunza.zunlog.repository.SubscriptionRepository
 import zunza.zunlog.repository.SseEmitterRepository
 import java.io.IOException
@@ -11,6 +20,7 @@ import java.io.IOException
 class NotificationService(
     private val sseEmitterRepository: SseEmitterRepository,
     private val subscriptionRepository: SubscriptionRepository,
+    private val notificationRepository: NotificationRepository,
 ) {
 
     fun subscribe(userId: Long): SseEmitter {
@@ -23,22 +33,26 @@ class NotificationService(
         return emitter
     }
 
-    fun notify(publisherId: Long) {
-        val subscriptions = subscriptionRepository.findByTargetId(publisherId)
+    fun notify(event: CustomEvent) {
+        val subscriptions = subscriptionRepository.findByTargetId(event.senderId)
         val subscribers = findConnectedSubscribers(subscriptions)
 
+        val notifications = mutableListOf<Notification>()
         subscribers.forEach { (subscriberId, emitter)  ->
+            val notification = Notification(senderId = event.senderId, receiverId = subscriberId, message = event.getMessage())
+            notifications.add(notification)
             try {
                 emitter.send(
                     SseEmitter.event()
-                        .name("new post")
-                        .data("${publisherId}님이 새 글을 등록했습니다!")
+                        .name(event.getEventName())
+                        .data(event.getMessage())
                 )
             } catch (e: IOException) {
                 emitter.complete()
                 sseEmitterRepository.deleteByUserId(subscriberId)
             }
         }
+        notificationRepository.saveAll(notifications)
     }
 
     private fun findConnectedSubscribers(subscriptions: List<Subscription>): Map<Long, SseEmitter> {
@@ -46,5 +60,23 @@ class NotificationService(
             val emitter = sseEmitterRepository.findBySubscriberId(subscription.subscriberId)
             emitter?.let { subscription.subscriberId to emitter }
         }.toMap()
+    }
+
+    fun getAllNotifications(userId: Long, pageable: Pageable): Page<NotificationDTO> {
+        return notificationRepository.findAll(pageable)
+            .map { NotificationDTO.from(it) }
+    }
+
+    fun getUnreadNotificationCount(userId: Long): UnreadNotificationCountDTO {
+        val count = notificationRepository.countUnreadNotifications(userId)
+        return UnreadNotificationCountDTO(count)
+    }
+
+    @Transactional
+    fun read(notificationId: Long) {
+        val notification = notificationRepository.findById(notificationId).orElseThrow {
+            throw NotificationNotFoundException()
+        }
+        notification.UpdateStatus()
     }
 }
