f4b6eaa01e5e82fd9717b747a80d1f75c88e9473
zunza <fksfks015@naver.com>
2024-09-02 19:59:50 +0900

    feat: 비동기 처리를 위한 @Async 애노테이션 추가

--- src/main/kotlin/zunza/zunlog/event/CustomEventListener.kt
+++ src/main/kotlin/zunza/zunlog/event/CustomEventListener.kt
@@ -1,9 +1,8 @@
 package zunza.zunlog.event
 
 import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
-import zunza.zunlog.repository.SubscriptionRepository
 import zunza.zunlog.service.NotificationService
 
 @Component
@@ -11,6 +10,7 @@ class CustomEventListener(
     private val notificationService: NotificationService,
 ) {
 
+    @Async
     @EventListener
     fun notifyNewPost(postEvent: PostEvent) {
         notificationService.notify(postEvent)
