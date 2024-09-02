1a75af7ea535d02d5bb260ae60e05a14999789ae
zunza <fksfks015@naver.com>
2024-09-02 20:01:32 +0900

    feat: 비동기 설정 추가 @Async 애노테이션을 그냥 사용하게 되면 스레드 풀을 이용하지 않기 때문에 ThreadPoolTaskExecutor를 사용한다.

---
+++ src/main/kotlin/zunza/zunlog/config/AsyncConfig.kt
@@ -0,0 +1,22 @@
+package zunza.zunlog.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+
+@EnableAsync
+@Configuration
+class AsyncConfig {
+
+    @Bean
+    fun executor(): ThreadPoolTaskExecutor {
+        return ThreadPoolTaskExecutor().apply {
+            corePoolSize = 5
+            maxPoolSize = 10
+            queueCapacity = 20
+            setThreadNamePrefix("custom-pool-")
+            initialize()
+        }
+    }
+}
