7495034004a9d1e18f6988efc4413dcfbacceca4
zunza <fksfks015@naver.com>
2024-09-02 21:53:16 +0900

    feat: CommentRepository 추가

---
+++ src/main/kotlin/zunza/zunlog/repository/CommentRepository.kt
@@ -0,0 +1,9 @@
+package zunza.zunlog.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import zunza.zunlog.model.Comment
+
+@Repository
+interface CommentRepository : JpaRepository<Comment, Long> {
+}
