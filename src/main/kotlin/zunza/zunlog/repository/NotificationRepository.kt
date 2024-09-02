2c06f098b7455b262a3d599fce6db881262f17c5
zunza <fksfks015@naver.com>
2024-09-02 20:08:30 +0900

    feat: 알림 페이지 처리 추가

--- src/main/kotlin/zunza/zunlog/repository/NotificationRepository.kt
+++ src/main/kotlin/zunza/zunlog/repository/NotificationRepository.kt
@@ -1,17 +1,48 @@
 package zunza.zunlog.repository
 
+import com.querydsl.core.types.Projections
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
 import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
+import zunza.zunlog.dto.NotificationDTO
 import zunza.zunlog.model.Notification
+import zunza.zunlog.model.QNotification
 
 @Repository
-interface NotificationRepository: JpaRepository<Notification, Long> {
+interface NotificationRepository: JpaRepository<Notification, Long>, NotificationRepositoryCustom {
 
     @Query("SELECT COUNT(n) " +
             "FROM Notification n " +
             "WHERE n.receiverId = :id " +
             "AND n.isRead = 'FALSE'")
     fun countUnreadNotifications(@Param("id") id: Long): Int
+}
+
+interface NotificationRepositoryCustom {
+
+    fun findByReceiverId(id: Long, pageable: Pageable): List<NotificationDTO>
+}
+
+class NotificationRepositoryCustomImpl : NotificationRepositoryCustom, QuerydslRepositorySupport(Notification::class.java) {
+    private val notification = QNotification.notification
+
+    override fun findByReceiverId(id: Long, pageable: Pageable): List<NotificationDTO> {
+        return from(notification)
+            .select(
+                Projections.constructor(
+                    NotificationDTO::class.java,
+                    notification.id,
+                    notification.message,
+                    notification.isRead,
+                )
+            )
+            .where(notification.receiverId.eq(id))
+            .orderBy(notification.createdDt.desc())
+            .offset(pageable.offset)
+            .limit(pageable.pageSize.toLong())
+            .fetch()
+    }
 }
