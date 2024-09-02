2103c67fc73a9ea4bdb42f010b4db56b7245a18a
zunza <fksfks015@naver.com>
2024-09-02 20:23:30 +0900

    feat: 작성자가 아닐 경우 예외처리 추가

---
+++ src/main/kotlin/zunza/zunlog/exception/AuthorMismatchException.kt
@@ -0,0 +1,16 @@
+package zunza.zunlog.exception
+
+class AuthorMismatchException(): CustomException(MESSAGE) {
+
+    companion object {
+        private const val MESSAGE = "작성자가 아닙니다."
+    }
+
+    init {
+        initErrorField("User ID")
+    }
+
+    override fun getStatusCode(): Int {
+        return 403
+    }
+}
