06f5e740d949a0d585898c0af5daa340e2c2ba9a
zunza <fksfks015@naver.com>
2024-09-17 23:13:06 +0900

    feat: 401에러 핸들러 추가

---
+++ src/main/kotlin/zunza/zunlog/config/handler/Http401Handler.kt
@@ -0,0 +1,34 @@
+package zunza.zunlog.config.handler
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.MediaType
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.web.AuthenticationEntryPoint
+import zunza.zunlog.dto.ErrorDTO
+import java.nio.charset.StandardCharsets
+
+class Http401Handler(
+    private val objectMapper: ObjectMapper
+) : AuthenticationEntryPoint {
+
+    override fun commence(
+        request: HttpServletRequest?,
+        response: HttpServletResponse?,
+        authException: AuthenticationException?
+    ) {
+        val errorDTO = ErrorDTO(
+            "로그인이 필요한 서비스입니다.",
+            "",
+            401
+        )
+
+        with(response!!) {
+            contentType = MediaType.APPLICATION_JSON_VALUE
+            characterEncoding = StandardCharsets.UTF_8.name()
+            status = errorDTO.code
+            objectMapper.writeValue(writer, errorDTO)
+        }
+    }
+}
