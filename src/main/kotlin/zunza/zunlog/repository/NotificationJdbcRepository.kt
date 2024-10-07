5cd49e0e29ae3082954c4c9b7f29e1c610addf8e
zunza <fksfks015@naver.com>
2024-10-07 19:52:51 +0900

    modify: 배치사이즈 2000으로 설정 사이즈 바꿔가면서 테스트코드 돌린 결과 기존에 알림 리스트 사이즈로 배치 사이즈를 설정한 것보다 성능이 향상됌

--- src/main/kotlin/zunza/zunlog/repository/NotificationJdbcRepository.kt
+++ src/main/kotlin/zunza/zunlog/repository/NotificationJdbcRepository.kt
@@ -10,25 +10,34 @@ import java.sql.PreparedStatement
 class NotificationJdbcRepository(
     private val jdbcTemplate: JdbcTemplate
 ) {
+    companion object {
+        private const val BATCH_SIZE: Int = 2000
+    }
+
     fun batchInsertNotifications(notifications: List<Notification>) {
 
         val sql = "INSERT INTO notification (sender_id, receiver_id, reference_id, message, is_read, created_dt) " +
             "VALUES (?, ?, ?, ?, ?, ?)"
 
-        jdbcTemplate.batchUpdate(sql, object : BatchPreparedStatementSetter {
-            override fun setValues(ps: PreparedStatement, i: Int) {
-                val notification = notifications[i]
-                ps.setLong(1, notification.senderId)
-                ps.setLong(2, notification.receiverId)
-                ps.setLong(3, notification.referenceId)
-                ps.setString(4, notification.message)
-                ps.setString(5, notification.isRead.name)
-                ps.setTimestamp(6, java.sql.Timestamp.from(notification.createdDt))
-            }
+        for (i in notifications.indices step BATCH_SIZE) {
+            val end = Math.min(i + BATCH_SIZE, notifications.size)
+            val batchList = notifications.subList(i, end)
 
-            override fun getBatchSize(): Int {
-                return notifications.size
-            }
-        })
+            jdbcTemplate.batchUpdate(sql, object : BatchPreparedStatementSetter {
+                override fun setValues(ps: PreparedStatement, i: Int) {
+                    val notification = notifications[i]
+                    ps.setLong(1, notification.senderId)
+                    ps.setLong(2, notification.receiverId)
+                    ps.setLong(3, notification.referenceId)
+                    ps.setString(4, notification.message)
+                    ps.setString(5, notification.isRead.name)
+                    ps.setTimestamp(6, java.sql.Timestamp.from(notification.createdDt))
+                }
+
+                override fun getBatchSize(): Int {
+                    return BATCH_SIZE
+                }
+            })
+        }
     }
 }
